6f3b7c7d0305756e1bee4896c1c182b93879c59d
moe <moerahmeh95@hotmail.com>
2021-05-18 20:06:28 +0300

    Added util functions for image util.

---
+++ src/Functions/util.js
@@ -0,0 +1,17 @@
+
+export const brightnessFactor = (rgb) => {
+  const r = rgb[0]
+  const g = rgb[1]
+  const b = rgb[2]
+  const maxBrightness = Math.sqrt(
+    (r * r) +
+    (g * g) +
+    (b * b)
+  )
+  const brightness = Math.sqrt(
+    (r * r) * 0.509 +
+    (g * g) * 1.000 +
+    (b * b) * 0.194
+  )
+  return brightness / maxBrightness
+}
